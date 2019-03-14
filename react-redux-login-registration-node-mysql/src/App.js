d20b108076d4d938b7db5cfebb48211555423a39
Ritika <37659027+ritikapatel21@users.noreply.github.com>
2019-03-14 10:27:51 +0530

    Update App.js

--- react-redux-login-registration-node-mysql/src/App.js
+++ react-redux-login-registration-node-mysql/src/App.js
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+
 import logo from './logo.svg';
 import './App.css';
+import Loginscreen from './Loginscreen'
 
+//injectTapEventPlugin()
 class App extends Component {
+
+  constructor(props){
+     super(props)
+     this.state = {
+       loginPage: [],
+       uploadScreen: []
+     }
+  }
+  
+  componentWillMount(){
+    const loginPage = []
+    loginPage.push(<Loginscreen parentContext={this}/>)
+    this.setState({
+      loginPage: loginP    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         {this.state.loginPage}
+         {this.state.uploadScreen}
       </div>
     );
   }
 }
 
 export default App;
-
-
