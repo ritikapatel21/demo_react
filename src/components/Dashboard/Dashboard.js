53595c5c3201277fe0080a4ef4767f13216b4516
Ritika <37659027+ritikapatel21@users.noreply.github.com>
2019-03-14 14:52:49 +0530

    Update Dashboard.js

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { checkCookie } from "../../helpers/cookie";
 
+
+import * as sessionActions from "../../actions/sessionActions";
+import LoginForm from '../../components/Login/LoginForm'
+
+
+const styles = {
+    fontFamily: "sans-serif",
+    textAlign: "center"
+};
 class Dashboard extends Component {
-    
+    state = { open: false }; 
+    onLogout = event => {
+        event.preventDefault();
+        this.setState({ loading: true });
+        this.props.actions.logOutUser(() => {
+            this.setState({ loading: false });
+            //this.props.history.push("/");
+        });
+    };   
     render() {
-        return (
-            <div className="content">
-            "Wel Come to Dashboard"
+        const { open } = this.state;
+        return <div id="wrapper">
+            <div style={styles}>
+                <h2>Welcome to You !!!</h2>                
+                <h2>Login Dashboard</h2>
+                <a href="/" onClick={this.onLogout}>Log out</a>
             </div>
-        );
+        </div>;
+    }
+};
+function mapStateToProps({ session }) {
+    return {
+        session
     }
 }
 
-export default Dashboard;
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(sessionActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
