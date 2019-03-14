f36df790a780d424469a65415a41bb5229507a54
Ritika <37659027+ritikapatel21@users.noreply.github.com>
2019-03-14 10:29:58 +0530

    Update index.js

--- react-redux-login-registration-node-mysql/src/index.js
+++ react-redux-login-registration-node-mysql/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import {
-    HashRouter,
-    Route,
-    Switch
-} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import store from './store/configureStore'
-import App from 'containers/Login/Login.js';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render((
-    <Provider store={store}>
-        <HashRouter>
-            <Switch>                               
-                <Route path="/" name="Home" component={App} />
-            </Switch>
-        </HashRouter>
-    </Provider>
-), document.getElementById('root'));
+serviceWorker.unregister();
